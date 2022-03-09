db00c48ba1a54fb0f45ee448d704fe28f5fbca7e
crow <corvusmacrorhynchosjaponensis@gmail.com>
2022-03-09 16:16:03 +0900

    コマンドの追加（未機能）

---
+++ src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/startsiegewarfare.java
@@ -0,0 +1,10 @@
+package com.hashibutogarasu.siege_warfare_plugin.Command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class startsiegewarfare {
+    public static void run(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        sender.sendMessage("Command, sample!");
+    }
+}
--- src/main/java/com/hashibutogarasu/siege_warfare_plugin/Siege_Warfare_Plugin.java
+++ src/main/java/com/hashibutogarasu/siege_warfare_plugin/Siege_Warfare_Plugin.java
@@ -1,5 +1,9 @@
 package com.hashibutogarasu.siege_warfare_plugin;
 
+import com.hashibutogarasu.siege_warfare_plugin.Command.setteam;
+import com.hashibutogarasu.siege_warfare_plugin.Command.startsiegewarfare;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Siege_Warfare_Plugin extends JavaPlugin {
@@ -8,7 +12,20 @@ public final class Siege_Warfare_Plugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("攻城戦プラグインを開始しました");
+    }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("setteam")){
+            setteam.run(sender,cmd,commandLabel,args);
+            return true;
+        }
+        if(cmd.getName().equalsIgnoreCase("startsiegewarfare")){
+            startsiegewarfare.run(sender,cmd,commandLabel,args);
+            return true;
+        }
+
+        return false;
     }
 
     @Override
@@ -16,4 +33,6 @@ public final class Siege_Warfare_Plugin extends JavaPlugin {
         // Plugin shutdown logic
         getLogger().info("攻城戦プラグインを停止しました");
     }
+
+
 }
