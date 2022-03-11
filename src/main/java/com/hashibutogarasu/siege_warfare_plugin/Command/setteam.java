5a6ad4b079397b22baef79fef360e8c9e3024c00
crow <corvusmacrorhynchosjaponensis@gmail.com>
2022-03-11 20:53:13 +0900

    一応ステップ形式にはできた(あとはプレイヤーの指定をするだけ)

--- src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/setteam.java
+++ src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/setteam.java
@@ -1,18 +1,29 @@
 package com.hashibutogarasu.siege_warfare_plugin.Command;
 
-
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import  net.md_5.bungee.api.chat.*;
-
+import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 
-import static org.bukkit.Bukkit.dispatchCommand;
-import static org.bukkit.Bukkit.getServer;
-
 public class setteam {
+
+    /**
+     * Players list in the server {@linkplain
+     * teamnext#run(CommandSender, Command, String, String[])}
+     */
+    @NotNull
+    public static Collection<? extends Player> players;
+
+    /**
+     * args[0]:Selection mode random:Random selection,custom:Custom selection
+     *
+     * @param sender
+     * @param cmd
+     * @param commandLabel
+     * @param args
+     * @return
+     */
     public static boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args){
         if(args.length != 0 && sender.isOp()){
             String mode = args[0];
@@ -23,13 +34,13 @@ public class setteam {
             }
 
             if(mode.matches("custom")){
-                Collection<? extends Player> players = sender.getServer().getOnlinePlayers();
+                players = sender.getServer().getOnlinePlayers();
                 sender.sendMessage("カスタムでチームを設定します");
                 sender.sendMessage("プレイヤー数：" + players.size());
 
-                getServer().dispatchCommand(getServer().getConsoleSender(),
-                        "tellraw " + sender.getName() + "[\"\",{\"text\":\"[\"},{\"text\":\"Playername\",\"color\":\"yellow\"},{\"text\":\"] \"},{\"text\":\"赤チームに追加\\u2690\",\"underlined\":true,\"color\":\"red\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/team join red Kara_su\"}},{\"text\":\" \"},{\"text\":\"青チームに追加\\u2690\",\"underlined\":true,\"color\":\"blue\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/team join blue Kara_su\"}},{\"text\":\" \"},{\"text\":\"チーム状態をリセット×\",\"underlined\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/team leave Kara_su\"}}]"
-                );
+                teamnext.count = 0;
+
+                teamnext.run(sender, cmd, commandLabel, args);
 
                 return true;
             }
--- src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/teamnext.java
+++ src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/teamnext.java
@@ -3,9 +3,30 @@ package com.hashibutogarasu.siege_warfare_plugin.Command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import static com.hashibutogarasu.siege_warfare_plugin.Command.setteam.players;
+import static org.bukkit.Bukkit.getServer;
+
 public class teamnext {
     public static int count = 0;
+
+    /**
+     * Send next rawtext to sender
+     * for文だと鯖にいるプレイヤーの選択テキストのすべてを一気に送って見にくくなるのでステップ形式にした
+     * @return void
+     */
     public static boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args){
+
+        String playername = "Kara_su";
+
+        getServer().dispatchCommand(getServer().getConsoleSender(),
+                "team join " + args[0] + " " + playername
+        );
+
+        getServer().dispatchCommand(getServer().getConsoleSender(),
+                "tellraw " + sender.getName() + " [\"\",{\"text\":\"[\"},{\"text\":\"" + playername + "\",\"color\":\"yellow\"},{\"text\":\"] \"},{\"text\":\"赤チームに追加\\u2690\",\"underlined\":true,\"color\":\"red\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/teamnext red " + playername + "\"}},{\"text\":\" \"},{\"text\":\"青チームに追加\\u2690\",\"underlined\":true,\"color\":\"blue\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/teamnext blue " + playername + "\"}},{\"text\":\" \"},{\"text\":\"チーム状態をリセット×\",\"underlined\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/team leave " + "Kara_su" + "\"}}]"
+        );
+
+        count++;
 
         return true;
     }
