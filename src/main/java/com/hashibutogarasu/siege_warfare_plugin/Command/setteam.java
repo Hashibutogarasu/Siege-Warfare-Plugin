244ab460a045bd6402083cff64c3375c4aa0d5d1
crow <corvusmacrorhynchosjaponensis@gmail.com>
2022-03-11 16:16:00 +0900

    見た目ヨシ！

--- src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/setteam.java
+++ src/main/java/com/hashibutogarasu/siege_warfare_plugin/Command/setteam.java
@@ -2,10 +2,6 @@ package com.hashibutogarasu.siege_warfare_plugin.Command;
 
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,7 +10,7 @@ import java.util.Collection;
 
 public class setteam {
     public static boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args){
-        if(args.length != 0 && !(sender instanceof Player)){
+        if(args.length != 0 && sender.isOp()){
             String mode = args[0];
             if(mode.matches("random")){
                 sender.sendMessage("ランダムでチームを設定します");
@@ -27,24 +23,16 @@ public class setteam {
                 sender.sendMessage("カスタムでチームを設定します");
                 sender.sendMessage("プレイヤー数：" + players.size());
 
-                //sender.sendMessage(ChatColor.RED + "Correct usage: /" + commandLabel + " <player>");
-
-                if(players.size() != 0){
-                    for(Player player : players){
-                            BaseComponent[] command = new ComponentBuilder()
-                                    .append("[").color(ChatColor.WHITE)
-                                    .append(player.getName()).color(ChatColor.YELLOW)
-                                    .append("]").color(ChatColor.WHITE)
-                                    .append("赤チームに追加⚐").color(ChatColor.RED).bold(true).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/team join red " + player.getName()))
-                                    .append("青チームに追加⚐").color(ChatColor.BLUE).bold(true).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/team join blue " + player.getName()))
-                                    .append("チーム状態をリセット×").color(ChatColor.WHITE).bold(true).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/team leave " + player.getName()))
-                                    .create();
-
-
-
-                            return true;
-                    }
-                }
+                sender.sendMessage(
+                        ChatColor.RESET + "[" + ChatColor.YELLOW
+                        + "PlayerName"
+                                + ChatColor.RESET + "] " + ChatColor.RED + ChatColor.UNDERLINE
+                        + "赤チームに追加⚐"
+                                + ChatColor.RESET + " " + ChatColor.BLUE + ChatColor.UNDERLINE
+                        + "青チームに追加⚐"
+                                + ChatColor.RESET + " " + ChatColor.WHITE + ChatColor.UNDERLINE
+                        + "チーム状態をリセット×"
+                );
 
                 return true;
             }
